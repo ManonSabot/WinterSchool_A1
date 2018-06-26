d2eba398982fd60832db343e37e1737edce18c37
ManonSabot <30618413+ManonSabot@users.noreply.github.com>
2018-06-26 16:13:24 +1000

    with compatibility line

--- extreme_SST_values.sh
+++ extreme_SST_values.sh
@@ -6,6 +6,10 @@
 # percentiles of SST. These can be looked at over a multi-year (with or)
 # without running mean), multi-year seasonal, or yearly period.
 #
+# You might have to run:
+# sed -i -e 's/\r$//' extreme_SST_values.sh
+# to make the script compliant across all linux systems
+#
 # Contact: manon sabot <m.e.b.sabot@gmail.com>
 #------------------------------------------------------------------------------
 
@@ -18,6 +22,7 @@ cdo ydrunpctl,10,20 ./SST_ANOM_ALL.nc ./minfile.nc ./maxfile.nc \
 cdo ydrunpctl,90,20 ./SST_ANOM_ALL.nc ./minfile.nc ./maxfile.nc \
 	./SST_90_yrs_20rm.nc
 rm ./minfile.nc ./maxfile.nc
+
 
 # multi year no running mean
 cdo timpctl,10 ./SST_ANOM_ALL.nc -timmin ./SST_ANOM_ALL.nc \
